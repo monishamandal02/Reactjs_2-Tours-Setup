ad727671b33ea34c762f7b2507a4a44e97e5992f
monishamandal02 <monishamandal0203@gmail.com>
2021-05-26 17:17:20 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monishamandal02/Reactjs2_Tours.git/precache-manifest.dfc3efc2b38621cf01243e24206a65cb.js"
+  "/Reactjs_2-Tours-Setup/precache-manifest.778fe125cd81cb91489519366d2882ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monishamandal02/Reactjs2_Tours.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Reactjs_2-Tours-Setup/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
